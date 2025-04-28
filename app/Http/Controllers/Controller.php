745dae64b8206ad3282cbc81cce87120f37f46a3
Jafar Daw <jafardaw003g@gmail.com>
2025-04-28 13:13:06 +0300

    Many To Many Relationship

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function update(Request $request, Post $post)
+{
+    $post->tags()->sync($request->tags); 
+    return redirect()->back()->with('success', 'تم تحديث الوسوم بنجاح');
+}
 }
